a438a68240e13bb8425249f0042ea6953c45c77c
Eddie Lee <eddie_lee@brown.edu>
2016-04-19 18:15:02 -0400

    Updated challenge table so that question_name is the primary key

---
+++ src/main/java/edu/brown/cs/deet/pageHandler/AdminHandler.java
@@ -0,0 +1,10 @@
+package edu.brown.cs.deet.pageHandler;
+
+/**
+ * Handles all of the Admin-related requests, such as adding a new challenge or
+ * editing a pre-existing challenge.
+ * @author el13
+ */
+public class AdminHandler {
+
+}
