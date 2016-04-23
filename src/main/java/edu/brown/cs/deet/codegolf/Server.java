739b9a239f60251844f985720c3046db75bdec93
Tyler <tyler_hakomori@brown.edu>
2016-04-23 19:35:08 -0400

    Some login stuff

--- src/main/java/edu/brown/cs/deet/codegolf/Server.java
+++ src/main/java/edu/brown/cs/deet/codegolf/Server.java
@@ -1,12 +1,24 @@
 package edu.brown.cs.deet.codegolf;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.io.StringWriter;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.SQLException;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import edu.brown.cs.deet.pageHandler.AdminHandler;
+import freemarker.template.Configuration;
 import spark.ExceptionHandler;
 import spark.ModelAndView;
 import spark.QueryParamsMap;
@@ -17,15 +29,13 @@ import spark.Spark;
 import spark.TemplateViewRoute;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.gson.Gson;
-
-import edu.brown.cs.deet.pageHandler.AdminHandler;
-import freemarker.template.Configuration;
-
 final class Server {
   private static final Gson GSON = new Gson();
   private static AdminHandler admin;
+  
+  private static String appID = "1559408461020162";
+  private static String loginRedirectURL = "http://localhost:4567/fblogin";
+  private static String appSecret = "9ffcf58f5f448a3e9e723537c476b5eb";
 
   /**
    * Private constructor for a Server. Never gets called.
@@ -69,6 +79,52 @@ final class Server {
 
     // Setup Spark Routes
     Spark.get("/game", new FrontHandler(), freeMarker);
+
+    Spark.get("/categories", (request, response) -> {
+      Map<String, Object> variables = ImmutableMap.of("title", "Categories");
+      return new ModelAndView(variables, "categories.ftl");
+    }, freeMarker);
+
+    Spark.get("/", (request, response) -> {
+      Map<String, Object> variables = ImmutableMap.of("title", "Home",
+        "loginURL", getFBURL());
+      return new ModelAndView(variables, "landing.ftl");
+    }, freeMarker);
+    
+    Spark.get("/fblogin", (request, response) -> {
+      String fbcode = request.queryParams("code");
+      
+      return handleFBLogin(fbcode, response);
+    });
+  }
+  
+  private static String handleFBLogin(String code, Response response) {
+    String accessTokenURL =
+      "https://graph.facebook.com/v2.3/oauth/access_token?client_id="
+      + appID + "&redirect_uri=" + loginRedirectURL + "&client_secret="
+      + appSecret + "&code=" + code;
+    
+    Map<String, String> tokenJSON = getJSONFromURL(accessTokenURL);
+    
+    if (!tokenJSON.containsKey("access_token")) {
+      response.status(500);
+      return "failed";
+    }
+          
+    String graphDataURL = "https://graph.facebook.com/v2.3/me?access_token="
+      + tokenJSON.get("access_token") + "&fields=id,name";
+    
+    Map<String, String> dataJSON = getJSONFromURL(graphDataURL);
+    
+    if (!dataJSON.containsKey("id")) {
+      response.status(500);
+      return "failed";
+    }
+    
+    String toReturn = String.format("Name: %s, ID: %s",
+      dataJSON.get("name"), dataJSON.get("id"));
+    
+    return toReturn;
   }
 
   /**
@@ -160,6 +216,32 @@ final class Server {
       Map<String, Object> variables =
           new ImmutableMap.Builder().put("success", success).build();
       return GSON.toJson(variables);
+    }
+  }
+
+  //TODO: deal with login and registration differently
+  private static String getFBURL() {
+    return "https://www.facebook.com/dialog/oauth?client_id="
+      + appID + "&redirect_uri=" + loginRedirectURL;
+  }
+  
+  private static Map<String, String> getJSONFromURL(String urlString) {
+    try {
+      // open a URL connection
+      URL url = new URL(urlString);
+      URLConnection connection = url.openConnection();
+      connection.connect();
+      
+      // read the URL connection into a json object
+      Reader reader = new InputStreamReader(connection.getInputStream());
+      BufferedReader buff = new BufferedReader(reader);
+      String line = buff.readLine();
+      Type stringStringMap = new TypeToken<Map<String, String>>(){}.getType();
+      Map<String, String> json = new Gson().fromJson(line, stringStringMap);
+      return json;
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      return ImmutableMap.of("error", e.getMessage());
     }
   }
 
